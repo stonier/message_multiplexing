fc4bbe50f3c75e117dbf0b0165d910fdd65024d8
Daniel Stonier <d.stonier@gmail.com>
2014-09-15 12:52:54 +0900

    nanomsg services (last was actually pipelines)

---
+++ mm_mux_demux/src/demos/nanomsg_pipeline.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file /mm_mux_demux/src/nanomsg_pipeline.cpp
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <assert.h>
+#include <cstdio>
+#include <ctime>
+#include <cstring>
+#include <unistd.h>
+#include <nanomsg/nn.h>
+#include <nanomsg/pipeline.h>
+#include <iostream>
+
+#define SOCKET_ADDRESS "inproc://a"
+
+int main ()
+{
+    int push1;
+    int push2;
+    int pull1;
+    int pull2;
+
+    /*  Test fan-out. */
+
+    // these were originally test_xxx from https://github.com/nanomsg/nanomsg/blob/master/tests/testutil.h
+    // only difference is they had error checking
+    push1 = nn_socket(AF_SP, NN_PUSH);
+    nn_bind(push1, SOCKET_ADDRESS);
+    pull1 = nn_socket(AF_SP, NN_PULL);
+    nn_connect (pull1, SOCKET_ADDRESS);
+    pull2 = nn_socket (AF_SP, NN_PULL);
+    nn_connect (pull2, SOCKET_ADDRESS);
+
+    /*  Wait till both connections are established to get messages spread
+        evenly between the two pull sockets. */
+    //nn_sleep(10);
+    sleep(1);
+
+    int length = strlen("ABC");
+    //test_send (push1, "ABC");
+    nn_send(push1, "ABC", length, 0);
+    //test_send (push1, "DEF");
+    nn_send(push1, "DEF", length, 0);
+
+    //test_recv (pull1, "ABC");
+    //test_recv (pull2, "DEF");
+    char *buf = NULL;
+    int bytes;
+    bytes = nn_recv(pull1, &buf, NN_MSG, 0);
+    assert (bytes >= 0);
+    printf ("PULL1: RECEIVED %s\n", buf);
+    bytes = nn_recv(pull2, &buf, NN_MSG, 0);
+    assert (bytes >= 0);
+    printf ("PULL2: RECEIVED %s\n", buf);
+
+    nn_close(push1);
+    nn_close(pull1);
+    nn_close(pull2);
+
+    /*  Test fan-in. */
+
+    pull1 = nn_socket (AF_SP, NN_PULL);
+    nn_bind(pull1, SOCKET_ADDRESS);
+    push1 = nn_socket (AF_SP, NN_PUSH);
+    nn_connect(push1, SOCKET_ADDRESS);
+    push2 = nn_socket(AF_SP, NN_PUSH);
+    nn_connect(push2, SOCKET_ADDRESS);
+
+    nn_send (push1, "ABC", length, 0);
+    nn_send (push2, "DEF", length, 0);
+
+    //test_recv (pull1, "ABC");
+    //test_recv (pull1, "DEF");
+    bytes = nn_recv (pull1, &buf, NN_MSG, 0);
+    assert (bytes >= 0);
+    printf ("PULL1: RECEIVED %s\n", buf);
+    bytes = nn_recv (pull1, &buf, NN_MSG, 0);
+    assert (bytes >= 0);
+    printf ("PULL2: RECEIVED %s\n", buf);
+
+    nn_close (pull1);
+    nn_close (push1);
+    nn_close (push2);
+
+    return 0;
+}
--- mm_mux_demux/src/demos/nanomsg_services.cpp
+++ mm_mux_demux/src/demos/nanomsg_services.cpp
@@ -1,89 +1,177 @@
 /**
- * @file /mm_mux_demux/src/nanomsg_services.cpp
+ * @file /mm_mux_demux/src/demos/nanomsg_services.cpp
+ * 
+ * @brief Short description of this file.
  **/
 /*****************************************************************************
 ** Includes
 *****************************************************************************/
 
-#include <assert.h>
 #include <cstdio>
+#include <cstdlib>
 #include <ctime>
 #include <cstring>
 #include <unistd.h>
 #include <nanomsg/nn.h>
-#include <nanomsg/pipeline.h>
+#include <nanomsg/reqrep.h>
 #include <iostream>
 
-#define SOCKET_ADDRESS "inproc://a"
+#define SOCKET_ADDRESS "inproc://test"
+
+void test_recv(int sock, const char *data) {
+  size_t data_length;
+  void* buf = NULL;
+  data_length = strlen(data);
+  int bytes;
+  bytes = nn_recv(sock, &buf, NN_MSG, 0);
+  printf ("Socket %d EXPECTED %s RECEIVED %s [%d bytes]\n", sock, data, (char*)buf, bytes);
+  nn_freemsg(buf);
+}
 
 int main ()
 {
-    int push1;
-    int push2;
-    int pull1;
-    int pull2;
+    int rc;
+    int rep1;
+    int rep2;
+    int req1;
+    int req2;
+    int resend_ivl;
+    char buf [7];
+    int timeo;
 
-    /*  Test fan-out. */
+    /*  Test req/rep with full socket types. */
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_bind (rep1, SOCKET_ADDRESS);
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_connect (req1, SOCKET_ADDRESS);
+    req2 = nn_socket (AF_SP, NN_REQ);
+    nn_connect (req2, SOCKET_ADDRESS);
 
-    // these were originally test_xxx from https://github.com/nanomsg/nanomsg/blob/master/tests/testutil.h
-    // only difference is they had error checking
-    push1 = nn_socket(AF_SP, NN_PUSH);
-    nn_bind(push1, SOCKET_ADDRESS);
-    pull1 = nn_socket(AF_SP, NN_PULL);
-    nn_connect (pull1, SOCKET_ADDRESS);
-    pull2 = nn_socket (AF_SP, NN_PULL);
-    nn_connect (pull2, SOCKET_ADDRESS);
+    /*  Check invalid sequence of sends and recvs. */
+    rc = nn_send (rep1, "ABC", 3, 0);
+    //nn_assert (rc == -1 && nn_errno () == EFSM);
+    rc = nn_recv (req1, buf, sizeof (buf), 0);
+    //nn_assert (rc == -1 && nn_errno () == EFSM);
 
-    /*  Wait till both connections are established to get messages spread
-        evenly between the two pull sockets. */
-    //nn_sleep(10);
-    sleep(1);
+    /*  Check fair queueing the requests. */
+    nn_send (req2, "ABC", 3, 0);
+    test_recv (rep1, "ABC");
+    nn_send (rep1, "ABC", 3, 0);
+    test_recv (req2, "ABC");
 
-    int length = strlen("ABC");
-    //test_send (push1, "ABC");
-    nn_send(push1, "ABC", length, 0);
-    //test_send (push1, "DEF");
-    nn_send(push1, "DEF", length, 0);
+    nn_send (req1, "ABC", 3, 0);
+    test_recv (rep1, "ABC");
+    nn_send (rep1, "ABC", 3, 0);
+    test_recv (req1, "ABC");
 
-    //test_recv (pull1, "ABC");
-    //test_recv (pull2, "DEF");
-    char *buf = NULL;
-    int bytes;
-    bytes = nn_recv(pull1, &buf, NN_MSG, 0);
-    assert (bytes >= 0);
-    printf ("PULL1: RECEIVED %s\n", buf);
-    bytes = nn_recv(pull2, &buf, NN_MSG, 0);
-    assert (bytes >= 0);
-    printf ("PULL2: RECEIVED %s\n", buf);
+    nn_close (rep1);
+    nn_close (req1);
+    nn_close (req2);
 
-    nn_close(push1);
-    nn_close(pull1);
-    nn_close(pull2);
+    /*  Check load-balancing of requests. */
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_bind (req1, SOCKET_ADDRESS);
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_connect (rep1, SOCKET_ADDRESS);
+    rep2 = nn_socket (AF_SP, NN_REP);
+    nn_connect (rep2, SOCKET_ADDRESS);
 
-    /*  Test fan-in. */
+    nn_send (req1, "ABC", 3, 0);
+    test_recv (rep1, "ABC");
+    nn_send (rep1, "ABC", 3, 0);
+    test_recv (req1, "ABC");
 
-    pull1 = nn_socket (AF_SP, NN_PULL);
-    nn_bind(pull1, SOCKET_ADDRESS);
-    push1 = nn_socket (AF_SP, NN_PUSH);
-    nn_connect(push1, SOCKET_ADDRESS);
-    push2 = nn_socket(AF_SP, NN_PUSH);
-    nn_connect(push2, SOCKET_ADDRESS);
+    nn_send (req1, "ABC", 3, 0);
+    test_recv (rep2, "ABC");
+    nn_send (rep2, "ABC", 3, 0);
+    test_recv (req1, "ABC");
 
-    nn_send (push1, "ABC", length, 0);
-    nn_send (push2, "DEF", length, 0);
+    nn_close (rep2);
+    nn_close (rep1);
+    nn_close (req1);
 
-    //test_recv (pull1, "ABC");
-    //test_recv (pull1, "DEF");
-    bytes = nn_recv (pull1, &buf, NN_MSG, 0);
-    assert (bytes >= 0);
-    printf ("PULL1: RECEIVED %s\n", buf);
-    bytes = nn_recv (pull1, &buf, NN_MSG, 0);
-    assert (bytes >= 0);
-    printf ("PULL2: RECEIVED %s\n", buf);
+    /*  Test re-sending of the request. */
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_bind (rep1, SOCKET_ADDRESS);
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_connect (req1, SOCKET_ADDRESS);
+    resend_ivl = 100;
+    rc = nn_setsockopt (req1, NN_REQ, NN_REQ_RESEND_IVL,
+        &resend_ivl, sizeof (resend_ivl));
 
-    nn_close (pull1);
-    nn_close (push1);
-    nn_close (push2);
+    nn_send (req1, "ABC", 3, 0);
+    test_recv (rep1, "ABC");
+    /*  The following waits for request to be resent  */
+    test_recv (rep1, "ABC");
+
+    nn_close (req1);
+    nn_close (rep1);
+
+    /*  Check sending a request when the peer is not available. (It should
+        be sent immediatelly when the peer comes online rather than relying
+        on the resend algorithm. */
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_connect (req1, SOCKET_ADDRESS);
+    nn_send (req1, "ABC", 3, 0);
+
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_bind (rep1, SOCKET_ADDRESS);
+    timeo = 200;
+    rc = nn_setsockopt (rep1, NN_SOL_SOCKET, NN_RCVTIMEO,
+       &timeo, sizeof (timeo));
+
+    nn_close (req1);
+    nn_close (rep1);
+
+    /*  Check removing socket request sent to (It should
+        be sent immediatelly to other peer rather than relying
+        on the resend algorithm). */
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_bind (req1, SOCKET_ADDRESS);
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_connect (rep1, SOCKET_ADDRESS);
+    rep2 = nn_socket (AF_SP, NN_REP);
+    nn_connect (rep2, SOCKET_ADDRESS);
+
+    timeo = 200;
+    rc = nn_setsockopt (rep1, NN_SOL_SOCKET, NN_RCVTIMEO,
+       &timeo, sizeof (timeo));
+    rc = nn_setsockopt (rep2, NN_SOL_SOCKET, NN_RCVTIMEO,
+       &timeo, sizeof (timeo));
+
+    nn_send (req1, "ABC", 3, 0);
+    /*  We got request through rep1  */
+    test_recv (rep1, "ABC");
+    /*  But instead replying we simulate crash  */
+    nn_close (rep1);
+    /*  The rep2 should get request immediately  */
+    test_recv (rep2, "ABC");
+    /*  Let's check it's delivered well  */
+    nn_send (rep2, "REPLY", 5, 0);
+    test_recv (req1, "REPLY");
+
+
+    nn_close (req1);
+    nn_close (rep2);
+
+    /*  Test cancelling delayed request  */
+
+    req1 = nn_socket (AF_SP, NN_REQ);
+    nn_connect (req1, SOCKET_ADDRESS);
+    nn_send (req1, "ABC", 3, 0);
+    nn_send (req1, "DEF", 3, 0);
+
+    rep1 = nn_socket (AF_SP, NN_REP);
+    nn_bind (rep1, SOCKET_ADDRESS);
+    timeo = 100;
+//    rc = nn_setsockopt (rep1, NN_SOL_SOCKET, NN_RCVTIMEO,
+//       &timeo, sizeof (timeo));
+//    errno_assert (rc == 0);
+    test_recv (rep1, "DEF");
+
+    nn_close (req1);
+    nn_close (rep1);
 
     return 0;
 }
+
